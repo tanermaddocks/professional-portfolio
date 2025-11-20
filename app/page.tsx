0b4b5e97403225ea811821203349d7297132d994
tanermaddocks <maddockstaner@gmail.com>
2025-11-20 10:31:19 +1000

    feat: Basic sections added to root page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      
+      <section>Gallery</section>
+      <section>
+        <h1> First Surname </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam, eum
+          illum, optio porro qui quibusdam architecto quia totam quos ipsam
+          autem sit molestias, eligendi recusandae labore debitis nisi! Rem!
+        </p>
+      </section>
+      {/* <ContactForm /> */}
     </main>
   );
 }
