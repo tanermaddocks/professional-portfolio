891c085d1a5dc714d7533e623be0e0c1886300d7
tanermaddocks <maddockstaner@gmail.com>
2025-11-20 12:24:17 +1000

    feat: Added adaptable content section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
+import ContactForm from "@/components/ContactForm";
+import ContentSection from "@/components/ContentSection";
 
 export default function Home() {
   return (
     <main>
       <section>Gallery</section>
-      <section>
-        <h1> First Surname </h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum nam, eum
-          illum, optio porro qui quibusdam architecto quia totam quos ipsam
-          autem sit molestias, eligendi recusandae labore debitis nisi! Rem!
-        </p>
-      </section>
-      {/* <ContactForm /> */}
+      <ContentSection />
+      <ContactForm />
     </main>
   );
 }
